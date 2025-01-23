c71d2916e9f8d50ce12ae91d0d7b82a59a4f89d4
pulkitflipkart <pulkit.sharma2@flipkart.com>
2025-01-23 17:04:21 +0530

    add: DBUtils

--- JEDI-GROUP_G-JAVA-FLIPFIT-POS/src/com/flipkart/dao/GymOwnerDaoImpl.java
+++ JEDI-GROUP_G-JAVA-FLIPFIT-POS/src/com/flipkart/dao/GymOwnerDaoImpl.java
@@ -1,15 +1,38 @@
 package com.flipkart.dao;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.List;
 
 import com.flipkart.bean.GymCentre;
 import com.flipkart.bean.Slot;
+import com.flipkart.utils.DBUtils;
 
 public class GymOwnerDaoImpl implements GymOwnerDao {
+	Connection connection = null;
+	PreparedStatement statement = null;
 	// Method to add a new gym
-    public boolean addGym(GymCentre gymCentre) {
+    public boolean addGym(GymCentre gymCenter) {
+    	try {
+			connection = DBUtils.getConnection();
+			statement = connection.prepareStatement(SQLQueries.INSERT_GYM);
+
+			
+			statement.setString(1,gymCenter.getName());
+			statement.setString(2,gymCenter.getLocation());
+			statement.setInt(3,0);
+			statement.executeUpdate();
+			statement.close();
+			
+	    } catch(SQLException sqlExcep) {
+		       System.out.println(sqlExcep);
+	    } catch(Exception excep) {
+	           excep.printStackTrace();
+	    }
         // Logic to add a gym
-        System.out.println("Gym added: " + gymCentre.getName());
+        System.out.println("Gym added: " + gymCenter.getName());
         return true; // Placeholder
     }
 
@@ -23,6 +46,24 @@ public class GymOwnerDaoImpl implements GymOwnerDao {
     // Method to add a new slot to a gym
     public boolean addSlotToGym(int gymId, Slot slot) {
         // Logic to add slot
+    	try {
+			connection = DBUtils.getConnection();
+			statement = connection.prepareStatement(SQLQueries.INSERT_SLOT);
+			
+			statement.setInt(1,gymId);
+			statement.setInt(2,slot.getSlotId());
+			statement.setString(3,slot.getStartTime());
+			statement.setString(4,slot.getEndTime());
+			statement.setInt(5,slot.getSeats());
+			statement.setInt(6,slot.getRemainingSeats());
+			statement.executeUpdate();
+			statement.close();
+			
+	    } catch(SQLException sqlExcep) {
+		       System.out.println(sqlExcep);
+	    } catch(Exception excep) {
+	           excep.printStackTrace();
+	    }	
         System.out.println("Slot added to gym ID: " + gymId);
         return true; // Placeholder
     }
@@ -30,6 +71,31 @@ public class GymOwnerDaoImpl implements GymOwnerDao {
     // Method to retrieve all gyms for an owner
     public List<GymCentre> getGymsForOwner(int ownerId) {
         // Logic to fetch gyms
-        return null; // Placeholder
+List<GymCentre> allGyms =  new ArrayList<>();
+		
+		try {
+			connection = DBUtils.getConnection();
+			statement = connection.prepareStatement(SQLQueries.FETCH_GYMOWNER_ALL_GYMS);
+			statement.setString(1,gymownerId());
+		    ResultSet rs = statement.executeQuery();
+			
+		    while (rs.next()) {
+		    	GymCentre gym = new GymCentre();
+				gym.setId(rs.getInt("id"));
+				gym.setName(rs.getString("name"));
+				gym.setLocation(rs.getString("location"));
+				gym.setNoOfSeats(rs.getInt("noOfSeats"));
+				gym.setGymOwnerEmail(rs.getString("gymOwnerEmail"));
+				gym.setApproved(rs.getBoolean("isApproved"));
+				allGyms.add(gym);
+			}
+	    } catch(SQLException sqlExcep) {
+		       System.out.println(sqlExcep);
+	    } catch(Exception excep) {
+	           excep.printStackTrace();
+	    }
+		
+		return allGyms;
+         // Placeholder
     }
 }
