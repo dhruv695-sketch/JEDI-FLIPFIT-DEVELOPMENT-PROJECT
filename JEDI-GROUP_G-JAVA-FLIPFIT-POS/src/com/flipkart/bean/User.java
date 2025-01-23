9719cc279fd86d0a3bfe4b7e2a122db171ffa8a2
Vamsi Krishna Gattupalli <vamsi.k@flipkart.com>
2025-01-23 16:48:07 +0530

    Update User.java

--- JEDI-GROUP_G-JAVA-FLIPFIT-POS/src/com/flipkart/bean/User.java
+++ JEDI-GROUP_G-JAVA-FLIPFIT-POS/src/com/flipkart/bean/User.java
@@ -7,7 +7,7 @@ public class User {
 	private int userId;
 	private String userName;
     private String userEmail;
-    private String userRoleId;
+    private int userRoleId;
     private String userPassword;
     
     /**
